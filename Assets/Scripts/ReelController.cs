aa8087dc77575546457fa3bcf1d86eb3eed7e4a0
Rohit Khatri <rohitkhatri1616@gmail.com>
2026-09-08 17:35:37 +0530

    Implemented infinite scroll logic and set up all 3 reels

---
+++ Assets/Scripts/ReelController.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReelController : MonoBehaviour
+{
+    [Header("References")]
+    public RectTransform symbolPrefab;
+    public SlotSymbol[] symbolData; 
+    
+    [Header("Settings")]
+    public float scrollSpeed = 2000f;
+    public float symbolHeight = 300f; // The vertical spacing between symbols
+    public int totalSymbols = 6;      // How many symbols to stack (covers window + buffer)
+    
+    private List<RectTransform> activeSymbols = new List<RectTransform>();
+    private bool isSpinning = false;
+
+    void Start()
+    {
+        InitializeReel();
+    }
+
+    void InitializeReel()
+    {
+        for (int i = 0; i < totalSymbols; i++)
+        {
+            // Spawn the prefab as a child of this Reel Window
+            RectTransform newSymbol = Instantiate(symbolPrefab, transform);
+            
+            // Stack them vertically (0, 300, 600, etc.)
+            newSymbol.anchoredPosition = new Vector2(0, i * symbolHeight);
+            
+            // Assign a random symbol from your data
+            AssignRandomSymbol(newSymbol);
+
+            activeSymbols.Add(newSymbol);
+        }
+    }
+
+    void Update()
+    {
+        // Temporary input for testing your milestone
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isSpinning = !isSpinning;
+        }
+
+        if (isSpinning)
+        {
+            SpinReel();
+        }
+    }
+
+    void SpinReel()
+    {
+        foreach (RectTransform symbol in activeSymbols)
+        {
+            // Move symbol down
+            symbol.anchoredPosition += Vector2.down * scrollSpeed * Time.deltaTime;
+
+            // If the symbol drops completely below the view
+            if (symbol.anchoredPosition.y <= -symbolHeight)
+            {
+                // Teleport it to the very top of the stack
+                symbol.anchoredPosition = new Vector2(0, symbol.anchoredPosition.y + (totalSymbols * symbolHeight));
+                
+                // Swap the picture so the pattern looks random
+                AssignRandomSymbol(symbol);
+            }
+        }
+    }
+
+    void AssignRandomSymbol(RectTransform symbol)
+    {
+        Image symbolImage = symbol.GetComponent<Image>();
+        SlotSymbol randomData = symbolData[Random.Range(0, symbolData.Length)];
+        symbolImage.sprite = randomData.symbolSprite;
+    }
+}
